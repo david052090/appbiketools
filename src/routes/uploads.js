e335b53fb73cc307dad03a4de2ec3ab28086f3e2
david052090 <barriosdavid310@gmil.com>
2022-08-26 14:57:12 -0500

    Se agrego usuario admin para registro de talleres

--- src/routes/uploads.js
+++ src/routes/uploads.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const multer = require("multer");
+const upload = multer();
 
 const router = Router();
+const type = upload.single("img");
 
-router.get("/uploadedimg", (req, res) => {
+router.get("/uploads", (req, res) => {
   res.render("./uploadsimg/uploadedimg");
+});
+router.post("/uploads", type, (req, res) => {
+  console.log(req);
 });
 module.exports = router;
 /////////////////////
