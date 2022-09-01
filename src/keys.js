4b4ae8a8da97bf0905931be7bad81cd0038ab3d0
david052090 <barriosdavid310@gmil.com>
2022-09-01 13:41:28 -0500

    se agregaron credenciales en el archivo keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bike_tools",
-    port: "3308",
+    host: "us-cdbr-east-06.cleardb.ne",
+    user: "b9e23c7e40fef7",
+    password: "74c6aecd",
+    database: "heroku_ec302d9a1a14448",
+    //port: "3308",
   },
 };
