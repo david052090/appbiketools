7dc941448f5c398ea9fcdbcc5054941eb7c0bcd8
david052090 <barriosdavid310@gmil.com>
2022-09-01 13:10:18 -0500

    se agrego conexion a la DB de heroku

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
-    host: "us-cdbr-east.cleardb.com",
-    user: "adffdadf2341",
-    password: "adf4234",
-    database: "heroku_db",
-    //port: "3308",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bike_tools",
+    port: "3308",
   },
 };
